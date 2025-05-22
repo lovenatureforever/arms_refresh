c8b24a16b017f129187f1ad1f5263a96d5de1455
lovenatureforever <lance20pacochawhi@hotmail.com>
2025-05-22 23:21:07 +0800

    add roles in audit-partners index

--- resources/views/livewire/tenant/pages/audit-partners/index-audit-partner.blade.php
+++ resources/views/livewire/tenant/pages/audit-partners/index-audit-partner.blade.php
@@ -20,6 +20,7 @@
                                     <th class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500" scope="col">User Name</th>
                                     <th class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500" scope="col">License No.</th>
                                     <th class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500" scope="col">Title</th>
+                                    <th class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500 max-w-[400px]" scope="col">Role</th>
                                     <th class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500" scope="col">Status</th>
                                     <th class="px-6 py-3 text-end text-xs font-medium uppercase text-gray-500" scope="col">Action</th>
                                 </tr>
@@ -32,6 +33,25 @@
                                         <td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800 dark:text-gray-200">{{ $auditor->user->username }}</td>
                                         <td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800 dark:text-gray-200">{{ $auditor->selected_license->license_no }}</td>
                                         <td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800 dark:text-gray-200">{{ $auditor->title }}</td>
+                                        <td class="px-6 py-4 text-sm font-medium text-gray-800 dark:text-gray-200 max-w-[400px] break-words">
+                                            @php
+                                                $systemRoles = $auditor->user->roles()
+                                                    ->where('name', 'like', 'internal%')->get()
+                                                    ->map(function($role) {
+                                                        return Str::substr(\App\Models\User::USER_ROLES[$role->name], 9);
+                                                    });
+                                                $isqmRoles = $auditor->user->roles()
+                                                    ->where('name', 'like', 'isqm%')->get()
+                                                    ->map(function($role) {
+                                                        return Str::substr(\App\Models\User::USER_ROLES[$role->name], 5);
+                                                    });
+                                            @endphp
+                                            <span class="font-bold">Internal:</span>
+                                            {{ implode(', ', $systemRoles->toArray()) }}
+                                            <br>
+                                            <span class="font-bold">ISQM:</span>
+                                            {{ implode(', ', $isqmRoles->toArray()) }}
+                                        </td>
                                         <td class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800 dark:text-gray-200">
                                             <div class="inline-flex items-center gap-1.5 py-1 px-3 rounded text-xs font-medium {{ $auditor->user->is_active ? 'bg-success/90' : 'bg-dark/80' }} text-white">{{ $auditor->user->is_active ? 'Active' : 'Inactive' }}</div>
                                         </td>
